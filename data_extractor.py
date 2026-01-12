bfcf58060694e988a97dce6ec92189c31710600f
titustang <titustang@gmail.com>
2026-01-12 12:06:00 +1100

    fixed backticks

--- data_extractor.py
+++ data_extractor.py
@@ -3,5 +3,7 @@ import pandas as pd
 
 df = pd.read_excel('OGHIST_2025_10_07.xlsx','Country Analytical History')
 s = df.set_index('Name').to_json(orient='index')
-print(s)
-# Copy the printed string into index.html
+# print(s)
+with open('data_as_json.json','w') as f:
+    f.write(s)
+# Copy the printed string into index.html in between backticks (``) just before json.parse()
